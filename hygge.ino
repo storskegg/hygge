ce5446c152b6c0d1db30d2b83aa49340a7a50eac
Liam Conrad <liam@w4pho.net>
2023-11-28 13:46:39 -0800

    revert eeprom bits

--- hygge.ino
+++ hygge.ino
@@ -19,31 +19,6 @@ void setup() {
   // Initialize all board peripherals, call this first
   ums3.begin();
 
-  if (EEPROM_RESET || hygge_last.msg_t != 1) {
-    // Serial.print("Clearing Last Hygge...");
-    hygge_last.seq = 0;
-    hygge_last.msg_t = 1;
-    hygge_last.hotp = 0;
-    hygge_last.batt = 0.0;
-    hygge_last.humi = 0.0;
-    hygge_last.temp = 0.0;
-
-    EEPROM.put(0, hygge_last);
-    if (!EEPROM.commit()) {
-      // Serial.println("FAIL");
-      // Serial.println("\tError occurred committing cleared struct to EEPROM.");
-      bail();
-    }
-
-    EEPROM.get(0, hygge_last_c);
-    if (hygge_last_c.msg_t != 1) {
-      // Serial.println("FAIL");
-      // Serial.println("\t!assert(put, read); Did not read what we wrote");
-      bail();
-    }
-    // Serial.println("OK");
-  }
-
   if (EEPROM_RESET) {
     bailSlow();
   }
